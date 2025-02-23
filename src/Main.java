6066b26789e30b9017874067ecccc6b89cb4d5e9
KarRz <reza11781@gmail.com>
2025-02-24 05:28:35 +0700

    feat: add solver in main class

--- src/Main.java
+++ src/Main.java
@@ -51,6 +51,45 @@ public class Main {
 
             if (pieces.size() != P) {
                 System.out.println("Jumlah blok terdata tidak sesuai dengan input");
+                return;
+            }
+            
+            Board board = new Board(N, M);
+            Solver solver = new Solver(board, pieces);
+            long startTime = System.currentTimeMillis();
+            boolean solve = solver.runSolver();
+            long endTIme = System.currentTimeMillis();
+            long executionTime = endTIme - startTime;
+            StringBuilder result = new StringBuilder();
+
+            if (solve) {
+                result.append("Solusi ditemukan!\n");
+                result.append(board.getBoardString(false)).append("\n");
+
+                System.out.println("\nSolusi ditemukan!");
+                System.out.println(board.getBoardString(true));
+            } else {
+                result.append("Solusi tidak ditemukan.\n");
+                System.out.println("Solusi tidak ditemukan.");
+            }
+
+            result.append("Waktu pencarian: ").append(executionTime).append(" ms\n");
+            result.append("Banyak kasus yang ditinjau: ").append(solver.getAttemptCount()).append("\n");
+            System.out.println("Waktu pencarian: " + executionTime + " ms\n");
+            System.out.println("Banyak kasus yang ditinjau: " + solver.getAttemptCount() + "\n");
+
+            System.out.println("Apakah anda ingin menyimpan solusi? (y/n)\n");
+            String save = userInput.nextLine().trim().toLowerCase();
+
+            if (save.equals("y")) {
+                System.out.println("Masukkan nama file output: ");
+                String outputFile = userInput.nextLine();
+                try (PrintWriter writer = new PrintWriter(outputFile)) {
+                    writer.println(result);
+                    System.out.println("Hasil telah disimpan di " + outputFile);
+                } catch (IOException e) {
+                    System.out.println("Gagal menyimpan file output: " + e.getMessage());
+                }
             }
         } catch (FileNotFoundException e) {
             System.out.println("File tidak ditemukan");
